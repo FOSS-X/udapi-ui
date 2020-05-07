09830ef5c1527b5b6d42c672b62b57ed25eed35d
Mohammed Ataaur Rahaman <ataago7@gmail.com>
2020-05-07 23:07:31 +0530

    index home scripts

---
+++ src/home/home.py
@@ -0,0 +1,23 @@
+# 
+#   home.py
+#   Home page
+#
+#   Created by FOSS-X UDAPI Desgin Team on 7/05/20.
+#   Copyright © 2020 FOSS-X. All rights reserved.
+#   
+
+from flask import Blueprint, render_template, request, redirect, url_for, session, flash
+
+home = Blueprint("home", __name__, static_folder="static", template_folder="templates")
+
+@home.route("/home")
+def homepage():
+    username = session["username"]
+    return render_template("home.html", username=username)
+
+@home.route("/logout")
+def logout():
+    session.pop("username", None)
+    session.pop("jwt", None)
+    flash("Logged out Successfully", 'info')
+    return redirect(url_for("index"))
